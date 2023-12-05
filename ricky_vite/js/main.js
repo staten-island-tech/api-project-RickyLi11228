43f81bafed017a58696408839bfddeee73a35e62
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-12-05 10:14:55 -0500

    ez imagine im so msart

--- ricky_vite/js/main.js
+++ ricky_vite/js/main.js
@@ -1,21 +1,7 @@
-const DOMSelectors = {
-    form: document.querySelector("#form"),
-    btn: document.getElementById("#btn"),
-}
-const URL = `https://coffee.alexflipnote.dev/random`;
-async function getData(URL) {
-try {
-    const response = await fetch(URL);
-    if(response.status !=200) {
-        throw new Error (response.statusText);
-    }
+const URLs = `https://randomfox.ca/floof/`;
+async function getData(URLs) {
+    const response = await fetch(URLs);
     const data = await response.json();
-    document.querySelector("img").src = data.src;
-} catch (error) {
-    console.log(error, "There was an error");
-    document.querySelector("h1").textContent = "Error"
+    document.querySelector("img").src = data.image;
 }
-}
-getData(URL);
-
-    
+getData(URLs);
