d96060db70d65639e925ac5c309771f639492f0a
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-12-06 10:15:23 -0500

    WORKS BUT TRASH API

--- ricky_vite/js/main.js
+++ ricky_vite/js/main.js
@@ -1,7 +1,15 @@
-const URLs = `https://randomfox.ca/floof/`;
+const URLs = `https://db.ygoprodeck.com/api/v7/cardinfo.php`;
 async function getData(URLs) {
+try {
     const response = await fetch(URLs);
+    if(response.status !=200) {
+        throw new Error (response.statusText);
+    }
     const data = await response.json();
-    document.querySelector("img").src = data.image;
+    document.querySelector("h1").textContent = data.data;
+} catch (error) {
+    console.log(error, "There was an error 🤓");
+    document.querySelector("h1").textContent = "Error 🤓🤓🤓"
+}
 }
 getData(URLs);
