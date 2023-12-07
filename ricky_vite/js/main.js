f91613cea6aa8e5f3ad059b96e3afad40bffa957
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-12-07 10:15:27 -0500

    WORKS SO EZ but not cards :(

--- ricky_vite/js/main.js
+++ ricky_vite/js/main.js
@@ -6,10 +6,26 @@ try {
         throw new Error (response.statusText);
     }
     const data = await response.json();
+    data.data.forEach((data)=> console.log(data.name));
     document.querySelector("h1").textContent = data.data;
+    const array = data.data
+    console.log(array)
+    function insertCards(arr){
+        arr.data.forEach((data) => {
+            DOMSelectors.column.insertAdjacentHTML(
+                "beforeend",
+                `<div class="card">
+                    <h3 class = "name">${data.name}</h3>
+                    <img src="${data.img}" class="img">
+                    <h4>Price: ${data.price}</h4> 
+                </div>`
+            )
+        });
+    }
+insertCards(data);
 } catch (error) {
     console.log(error, "There was an error 🤓");
     document.querySelector("h1").textContent = "Error 🤓🤓🤓"
 }
 }
-getData(URLs);
+getData(URLs); 
